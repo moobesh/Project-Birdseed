1fc6f7568c20b61fd549de48d240afc4465f69bf
moobesh <35742358+moobesh@users.noreply.github.com>
2018-12-08 16:43:08 +0000

    create table and db

---
+++ create tables tele_mgmt.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE tele_mgmt
+
+CREATE TABLE profile(
+		id INTEGER NOT NULL AUTO_INCREMENT, 
+		name TEXT, 
+		extention INTEGER, 
+		type VARCHAR(255), 
+		PRIMARY KEY(id)
+);
+
+CREATE TABLE cab(
+
+		cab_id INTEGER NOT NULL,
+		cab INTEGER,
+		shelf INTEGER,
+		slot INTEGER,
+		circuit INTEGER,
+		patch TEXT
+
+);
+
+CREATE TABLE cer_vert(
+
+		vertical_id INTEGER NOT NULL,
+		vertical_location INTEGER,
+		vertical_sub_id INTEGER,
+		vertical_patch INTEGER,
+
+);
+
+CREATE TABLE per_vert(
+
+		per_vert_location INTEGER,
+		per_vert_port INTEGER
+
+);
+
+CREATE TABLE floorbox(
+
+		floorbox_id INTEGER,
+		floorbox_per TEXT,
+		floorbox_level TEXT,
+		floorbox_port INTEGER
+
+);
+
+CREATE TABLE logs(
+
+		post_id INTEGER,
+		post_timestamp INTEGER,
+		poster_initials TEXT,
+		post_info TEXT
+
+);
