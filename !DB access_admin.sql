5eb6aaaede549e84c00ce405f90593b5f2dd0b64
Pi3Man92 <45716912+Pi3Man92@users.noreply.github.com>
2018-12-14 18:42:20 +0000

    Create !DB access_admin.sql

---
+++ !DB access_admin.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE access_admin;
+CREATE TABLE access (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, first_name VARCHAR(25), last_name VARCHAR(25), username VARCHAR(25), passwd VARCHAR(25) );
+INSERT INTO access VALUES(NULL,"Charlton","Bellington","charlieboy21","Ch@rlie!");
